e82bc1145a2712d9a20d3b103a20076d21386783
Heather Ward <heather@dnastack.com>
2022-04-13 23:56:42 -0400

    Add TOC to services

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -3,131 +3,156 @@
     <div id="services_header">
       <h3>Services</h3>
     </div>
-    <div id="services_content">
-      <div>
-        <h4>Residential Services</h4>
-        <p>
-          When it comes to the home, there's no job that IHS Electric can't do.
-          We've helped countless homeowners in Stratford just like you that need
-          everything from small outlet repairs to complete electrical system
-          upgrades for older homes. Whatever you need, we'll provide a solution
-          built to last for many years to come.
-        </p>
-        <p>
-          In addition to home electrical installations, we are also able to
-          diagnose and repair residential problems, such as broken outlets,
-          excessive energy consumption, and flickering lights. For all repairs,
-          we'll address the underlying issue for a complete solution instead of
-          an electrical patch job that will need further fixes down the road.
-        </p>
-        <p>
-          When you choose IHS Electric, you're getting quality you can trust.
-          Call 519-301-5606 to discuss your home's needs.
-        </p>
+    <div id="services_content_container">
+      <div id="services_toc">
+        <ul>
+          <a href="#residential-services"><li>Residential Services</li></a>
+          <a href="#commercial-services"><li>Commercial Services</li></a>
+          <a href="#new-installations"><li>New Installations</li></a>
+          <a href="#inspections"><li>Inspections</li></a>
+          <a href="#lighting"><li>Lighting</li></a>
+        </ul>
       </div>
-      <br />
-      <div>
-        <h4>Commercial Services</h4>
-        <p>
-          The bigger your commercial property, the more complicated its
-          electrical system can be. With IHS Electric, you have a local partner
-          that understands what your business needs and how to execute
-          electrical work that will not only look great but function well. All
-          of our services can scale up or down, and we’re up for the challenge,
-          big or small.
-        </p>
-        <p>
-          Whether it's detailed electrical plans for a new construction or an
-          urgent electrical repair, IHS Electric is the one to call. Dial
-          519-301-5606 and rest assured that you’re in good hands!
-        </p>
-      </div>
-      <br />
-      <div>
-        <h4>New Installations</h4>
-        <p>
-          For residential and commercial clients, much of our work revolves
-          around new installations, whether it's washers and dryers for the home
-          or a piece of industrial equipment for a factory. Regardless of what's
-          being installed or where we know how to evaluate its power needs and
-          make sure the installation is done correctly.
-        </p>
-        <p>
-          The reason why it's a good idea to leave new installations to the
-          professionals at IHS Electric is that an improper installation can
-          lead to many different problems, ranging from excessive energy
-          consumption or appliance failure to an overloaded circuit that can
-          create a dangerous situation. With us, you can be sure that your
-          electrical system is safe and that your investment is protected.
-        </p>
-        <p>
-          All of our new electrical installations come with the signature IHS
-          Electric quality. For the work at hand, we'll always use the highest
-          grade of materials available, and from our suppliers, you’re sure to
-          get a great price.
-        </p>
-        <p>
-          IHS Electric is a Stratford name you can trust for new installations.
-          Call us today at 519-301-5606.
-        </p>
-      </div>
-      <br />
-      <div>
-        <h4>Inspections</h4>
-        <p>
-          If you've noticed common problems like power surges, repeatedly
-          tripped breakers, or higher than average electricity bills, there's a
-          good chance that your property needs a professional electrical
-          inspection. Affordable and detail-oriented, IHS Electric can provide
-          thorough inspections to discover even the most minute issues that
-          could be causing electrical problems.
-        </p>
-        <p>
-          While much of our inspection and troubleshooting work involves finding
-          a solution for redundant wiring or overloaded circuits, IHS Electric
-          is also available for routine inspections related to commercial
-          safety, home maintenance, rental units, and property sales. A clean
-          bill of health from an electrical contractor ensures that your
-          property's safety and value are always kept at their utmost potential.
-        </p>
-        <p>
-          Local businesses are often required to have electrical inspections on
-          a regular basis by a third party. We currently provide this service to
-          many companies in the area and can assist your property stay in
-          compliance with electrical regulations.
-        </p>
-        <p>
-          For a qualified electrical inspection from IHS Electric, call
-          519-301-5606 and schedule a visit to your property.
-        </p>
-      </div>
-      <br />
-      <div>
-        <h4>Lighting</h4>
-        <p>
-          In the past few years, advances in lighting technology have moved at a
-          rapid pace, which is good news for your hydro bill! At IHS Electric,
-          we pay close attention to the new developments and are perfectly
-          equipped to install state of the art LED lighting.
-        </p>
-        <p>
-          The benefit of this new technology is that while initial expenses may
-          be higher, the long-term cost is actually much lower. Combined with an
-          expert installation or upgrade from IHS Electric, your space will
-          receive quite a few benefits, from better light quality to lowered
-          energy consumption and a first-class installation that will stand up
-          for many years to come. And no more changing light bulbs!
-        </p>
-        <p>
-          In addition to new lighting fixtures and upgrades, IHS Electric is
-          also available to handle other tasks as well, such as maintenance and
-          repair of outdoor and pool lighting. With access to a wide range of
-          lighting fixture providers, we'll help you find the exact models you
-          need for your home or business.
-        </p>
-        <p>
-          Call 519-301-5606 to talk to us and bring new light to your location.
-        </p>
+      <div id="services_content">
+        <div>
+          <section id="residential-services">
+            <h4>Residential Services</h4>
+            <p>
+              When it comes to the home, there's no job that IHS Electric can't
+              do. We've helped countless homeowners in Stratford just like you
+              that need everything from small outlet repairs to complete
+              electrical system upgrades for older homes. Whatever you need,
+              we'll provide a solution built to last for many years to come.
+            </p>
+            <p>
+              In addition to home electrical installations, we are also able to
+              diagnose and repair residential problems, such as broken outlets,
+              excessive energy consumption, and flickering lights. For all
+              repairs, we'll address the underlying issue for a complete
+              solution instead of an electrical patch job that will need further
+              fixes down the road.
+            </p>
+            <p>
+              When you choose IHS Electric, you're getting quality you can
+              trust. Call 519-301-5606 to discuss your home's needs.
+            </p>
+          </section>
+        </div>
+        <br />
+        <div>
+          <section id="commercial-services">
+            <h4>Commercial Services</h4>
+            <p>
+              The bigger your commercial property, the more complicated its
+              electrical system can be. With IHS Electric, you have a local
+              partner that understands what your business needs and how to
+              execute electrical work that will not only look great but function
+              well. All of our services can scale up or down, and we’re up for
+              the challenge, big or small.
+            </p>
+            <p>
+              Whether it's detailed electrical plans for a new construction or
+              an urgent electrical repair, IHS Electric is the one to call. Dial
+              519-301-5606 and rest assured that you’re in good hands!
+            </p>
+          </section>
+        </div>
+        <br />
+        <div>
+          <section id="new-installations">
+            <h4>New Installations</h4>
+            <p>
+              For residential and commercial clients, much of our work revolves
+              around new installations, whether it's washers and dryers for the
+              home or a piece of industrial equipment for a factory. Regardless
+              of what's being installed or where we know how to evaluate its
+              power needs and make sure the installation is done correctly.
+            </p>
+            <p>
+              The reason why it's a good idea to leave new installations to the
+              professionals at IHS Electric is that an improper installation can
+              lead to many different problems, ranging from excessive energy
+              consumption or appliance failure to an overloaded circuit that can
+              create a dangerous situation. With us, you can be sure that your
+              electrical system is safe and that your investment is protected.
+            </p>
+            <p>
+              All of our new electrical installations come with the signature
+              IHS Electric quality. For the work at hand, we'll always use the
+              highest grade of materials available, and from our suppliers,
+              you’re sure to get a great price.
+            </p>
+            <p>
+              IHS Electric is a Stratford name you can trust for new
+              installations. Call us today at 519-301-5606.
+            </p>
+          </section>
+        </div>
+        <br />
+        <div>
+          <section id="inspections">
+            <h4>Inspections</h4>
+            <p>
+              If you've noticed common problems like power surges, repeatedly
+              tripped breakers, or higher than average electricity bills,
+              there's a good chance that your property needs a professional
+              electrical inspection. Affordable and detail-oriented, IHS
+              Electric can provide thorough inspections to discover even the
+              most minute issues that could be causing electrical problems.
+            </p>
+            <p>
+              While much of our inspection and troubleshooting work involves
+              finding a solution for redundant wiring or overloaded circuits,
+              IHS Electric is also available for routine inspections related to
+              commercial safety, home maintenance, rental units, and property
+              sales. A clean bill of health from an electrical contractor
+              ensures that your property's safety and value are always kept at
+              their utmost potential.
+            </p>
+            <p>
+              Local businesses are often required to have electrical inspections
+              on a regular basis by a third party. We currently provide this
+              service to many companies in the area and can assist your property
+              stay in compliance with electrical regulations.
+            </p>
+            <p>
+              For a qualified electrical inspection from IHS Electric, call
+              519-301-5606 and schedule a visit to your property.
+            </p>
+          </section>
+        </div>
+        <br />
+        <div>
+          <section id="lighting">
+            <h4>Lighting</h4>
+            <p>
+              In the past few years, advances in lighting technology have moved
+              at a rapid pace, which is good news for your hydro bill! At IHS
+              Electric, we pay close attention to the new developments and are
+              perfectly equipped to install state of the art LED lighting.
+            </p>
+            <p>
+              The benefit of this new technology is that while initial expenses
+              may be higher, the long-term cost is actually much lower. Combined
+              with an expert installation or upgrade from IHS Electric, your
+              space will receive quite a few benefits, from better light quality
+              to lowered energy consumption and a first-class installation that
+              will stand up for many years to come. And no more changing light
+              bulbs!
+            </p>
+            <p>
+              In addition to new lighting fixtures and upgrades, IHS Electric is
+              also available to handle other tasks as well, such as maintenance
+              and repair of outdoor and pool lighting. With access to a wide
+              range of lighting fixture providers, we'll help you find the exact
+              models you need for your home or business.
+            </p>
+            <p>
+              Call 519-301-5606 to talk to us and bring new light to your
+              location.
+            </p>
+          </section>
+        </div>
       </div>
     </div>
   </div>
@@ -139,13 +164,38 @@ export default {
 };
 </script>
 
-<style>
+<style scopes>
 #services_header,
-#services_content {
+#services_content,
+#services_toc {
   text-align: left;
 }
 
+#services_toc,
 #services_content {
   font-size: 70%;
+}
+
+#services_content_container {
+  display: flex;
+  gap: 15px;
+  flex-wrap: wrap;
+}
+
+#services_toc {
+  flex: 1;
+}
+
+#services_content {
+  flex: 2;
+}
+
+ul {
+  padding: 0;
+}
+
+#services_toc li {
+  padding: 15px 0;
+  text-decoration: underline;
 }
 </style>
